a39edcc4a8fa20539197421617fd2621a961aeb8
600408 <600408@syswin.com>
2018-04-25 18:34:24 +0800

    tab -> four space

--- installapk.py
+++ installapk.py
@@ -17,41 +17,41 @@ import mobileinfo
 
 #批量安装apk
 def install_apk(device):
-	apklist = []
-	apkdir = "apk"
-	if len(sys.argv) == 1:	
-		for f in os.listdir(apkdir):
-			extname = os.path.splitext(f)[1]
-			if extname == ".apk" :
-				apklist.append(f)
-		for apk in apklist:
-			print "install "+apk+" on "+device
-			res = os.system("adb -s " + device + " install -r "+ apkdir + "/" + "\"" + apk + "\"")
-			res = res>>8
-			if res != 0:
-				print "Failed to install " + apk +"\n"
-				return False
-			else:
-				print "Success to install " + apk +"\n"
-	elif len(sys.argv) > 1:
-		argvlen = len(sys.argv)
-		for l in xrange(argvlen):
-			if l != 0 :
-				apk = sys.argv[l]
-				print "install "+apk+" on "+device
-				res = os.system("adb -s " + device + " install -r "+ apkdir + "/" + "\"" + apk + "\"")
-				res = res>>8
-				if res != 0:
-					print "Failed to install " + apk +"\n"
-					return False
-				else:
-					print "Success to install " + apk +"\n"
+    apklist = []
+    apkdir = "apk"
+    if len(sys.argv) == 1:    
+        for f in os.listdir(apkdir):
+            extname = os.path.splitext(f)[1]
+            if extname == ".apk" :
+                apklist.append(f)
+        for apk in apklist:
+            print "install "+apk+" on "+device
+            res = os.system("adb -s " + device + " install -r "+ apkdir + "/" + "\"" + apk + "\"")
+            res = res>>8
+            if res != 0:
+                print "Failed to install " + apk +"\n"
+                return False
+            else:
+                print "Success to install " + apk +"\n"
+    elif len(sys.argv) > 1:
+        argvlen = len(sys.argv)
+        for l in xrange(argvlen):
+            if l != 0 :
+                apk = sys.argv[l]
+                print "install "+apk+" on "+device
+                res = os.system("adb -s " + device + " install -r "+ apkdir + "/" + "\"" + apk + "\"")
+                res = res>>8
+                if res != 0:
+                    print "Failed to install " + apk +"\n"
+                    return False
+                else:
+                    print "Success to install " + apk +"\n"
 
 def main():
-	mounted_devices = mobileinfo.find_devices()
-	if mounted_devices is not None:
-		for device in mounted_devices:
-			install_apk(device)
+    mounted_devices = mobileinfo.find_devices()
+    if mounted_devices is not None:
+        for device in mounted_devices:
+            install_apk(device)
 
 if __name__ == '__main__':
-	main()
+    main()
